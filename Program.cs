adbc3f3af8a26f541f353277a1a37f9d72faef27
Israel4301 <israelespinoza159@gmail.com>
2026-09-09 09:11:17 -0700

    Innital commit day 2 lecture

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Day two variable lecture
+//console.Writeline prints out to our terminal.
+//console.clear clears out terminal
+Console.WriteLine("Welcome to day two of the combine");
+Console.Write("Enter your name: ");
+
+
+
+
+
+
+//this allows the user to type into the console
+Console.ReadLine();
+
+//What is a variable?
+
+//A variable is a Box that can store values.
+//There several Data Types within C#
+
+//to declare a string variable we need to type string before the variable name and then assign it a value.
+string album = "Love Deluxe"; //we are initializing a string with a value.
+string name; //declaring a string we are not giving it value.
+
+
+//number Data Types
+//intergers as whole numbers
+int age = 36;
+
+//double can go up to 15-17 digits
+double chipotleBurrito = 11.45;
+
+
+//Boolean Data Type represents
+bool isItColdInHere = true; //Camel Case is when you have multiple words in a variable name first word is lowercase.
+
+Console.WriteLine(album);
+
+Console.WriteLine(isItColdInHere);
